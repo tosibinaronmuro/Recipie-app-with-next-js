0ab48f274750fe3083de392e0460845b59a5e9f6
Tosibinaronmuro <tosironj@gmail.com>
2022-05-21 16:16:15 +0100

    created a 404 page

---
+++ pages/404.js
@@ -0,0 +1,34 @@
+import React from "react";
+import Link from "next/link";
+import Head from "next/head";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
+function NotFound() {
+  const router = useRouter();
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     router.push("/");
+  //   }, 5000);
+  // }, []);
+  return (
+    <>
+      <Head>
+        <title>{"Tosiron's recipe | 404"}</title>
+        <meta name="keywords" content="Tosiron" />
+      </Head>
+      <div className="not-found bg-last ">
+        <h1 className="text-2xl text-secondary font-frank font-bold">Ooooops...</h1>
+        <h2 className="text-lg text-secondary font-rubik font-bold">That page cannot be found</h2>
+        <p>
+          go back to the{" "}
+          <Link href="/">
+            <a>Homepage</a>
+          </Link>
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default NotFound;
