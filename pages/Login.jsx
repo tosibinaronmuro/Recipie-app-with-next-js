ae979a79e2faa7dfcf2eae1c451e2c398d8f8452
Tosibinaronmuro <tosironj@gmail.com>
2022-05-27 20:47:14 +0100

    k

--- pages/Login.jsx
+++ pages/Login.jsx
@@ -95,9 +95,9 @@ function Login() {
         </Link>
         <div className="flex   mb-6">
           <div className="flex  h-5">
-            <p className=" text-sm font-medium flex justify-center items-center text-secondary ml-5  lg:ml-20  ">
+            <p className=" text-sm font-medium flex justify-center items-center text-secondary ml-5  lg:ml-20 ">
               don't have an account?   
-              <Link href='/Signup' ><span
+              <Link href="/Signup" ><span
                 
                 className="text-blue-600 hover:underline dark:text-blue-500"
               >
